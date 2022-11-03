75f1f21c756487a8d5e3c2cad43d1d560a5651b8
oxmox <martishkaoxanka@gmail.com>
2022-11-03 11:57:15 +1000

    3/4

--- File Server/task/src/client/Main.java
+++ File Server/task/src/client/Main.java
@@ -2,24 +2,53 @@ package client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        var msg = "Give me everything you have!";
+        var sc = new Scanner(System.in);
         System.out.println("Client started!");
-        try {
-            var socket = new Socket(InetAddress.getByName("127.0.0.1"), 23456);
-            new DataOutputStream(socket.getOutputStream())
-                    .writeUTF(msg);
-            System.out.println("Sent: " + msg);
-            System.out.println("Received: " +
-                    new DataInputStream(socket.getInputStream())
-                            .readUTF());
-        } catch (IOException e) {
+        try (var socket = new Socket(
+                InetAddress.getByName("127.0.0.1"),
+                23456);
+             var output = new DataOutputStream(socket.getOutputStream())) {
+            System.out.println("Enter action (" +
+                    "1 - get a file, " +
+                    "2 - create a file," +
+                    " 3 - delete a file): ");
+            var command = sc.nextLine();
+            if (!command.equals("exit")) {
+                System.out.println("Enter filename: ");
+                var fileName = sc.nextLine();
+
+                if (!command.equals("2")) {
+                    output.writeUTF(command +
+                            "==" + fileName);
+                } else {
+                    System.out.println("Enter file content: ");
+                    output.writeUTF(command +
+                            "==" +
+                            fileName +
+                            "==" +
+                            sc.nextLine());
+                }
+                System.out.println("The request was sent.");
+                var in = new DataInputStream(
+                                    socket.getInputStream())
+                                .readUTF();
+                System.out.println(
+                        command.equals("1") &&
+                        !in.equals("the file was not found!") ?
+                        "The content of the file is: " + in
+                        :
+                        "The response says that " + in);
+            } else {
+                System.out.println("The request was sent.");
+                output.writeUTF(command);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- File Server/task/src/server/Main.java
+++ File Server/task/src/server/Main.java
@@ -2,26 +2,66 @@ package server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.InetAddress;
+import java.io.FileWriter;
 import java.net.ServerSocket;
+import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        var str = "All files were sent!";
         System.out.println("Server started!");
-        try (var server = new ServerSocket(23456,
-                50,
-                InetAddress.getByName("127.0.0.1"))){
-            var socket = server.accept();
-            var output  = new DataOutputStream(socket.getOutputStream());
-            var msg = new DataInputStream(socket.getInputStream())
-                    .readUTF();
-            output.writeUTF(str);
-            System.out.println("Received: " + msg);
-            System.out.println("Sent: " + str);
-        } catch (IOException e) {
+        Path path = Path.of(System.getProperty("user.dir"),
+                "src",
+                "server",
+                "data");
+        System.out.println(path
+                         .toFile()
+                         .mkdirs());
+        String[] request = {"", null};
+        try (var server = new ServerSocket(23456)) {
+            while (!request[0].equals("exit")) {
+                var socket = server.accept();
+                var output = new DataOutputStream(socket.getOutputStream());
+                request = new DataInputStream(socket.getInputStream())
+                        .readUTF()
+                        .split("==");
+                if (!request[0].equals("exit")) {
+                    var file = Path.of(path + "/" + request[1]).toFile();
+                    switch (request[0]) {
+                        case "1" -> {
+                            if (file.exists()) {
+                                var sc = new Scanner(file);
+                                var sb = new StringBuilder();
+                                while (sc.hasNextLine())
+                                    sb.append(sc.nextLine());
+                                output.writeUTF(sb.toString());
+                            } else {
+                                output.writeUTF("the file was not found!");
+                            }
+                        }
+                        case "2" -> {
+                            if (file.createNewFile()) {
+                                var writer = new FileWriter(file);
+                                writer.write(request[2]);
+                                writer.close();
+                                output.writeUTF("file was created!");
+                            } else {
+                                output.writeUTF(
+                                        "creating the file was forbidden!");
+                            }
+                        }
+                        case "3" -> {
+                            if (file.delete()) {
+                                output.writeUTF(
+                                        "the file was successfully deleted!");
+                            } else {
+                                output.writeUTF("the file was not found!");
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
